46679e2e178d6f583efae68523eedfce054fd566
garvbahal <garvbahal75@gmail.com>
2026-08-07 16:32:52 +0530

    added-prisma

---
+++ packages/db/src/index.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client.js";
+dotenv.config();
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("Database URL is missing");
+}
+
+const adapter = new PrismaPg({ connectionString });
+
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
