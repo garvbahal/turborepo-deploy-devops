68e0ed83414f0d32bffd2b33deb5546f1fface87
garvbahal <garvbahal75@gmail.com>
2026-08-07 23:41:41 +0530

    verifying

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,8 +4,10 @@ export default async function Home() {
   const user = await prisma.user.findFirst();
   return (
     <div>
+      First name:
       {user?.username}
       <br />
+      Password:
       {user?.password}
     </div>
   );
